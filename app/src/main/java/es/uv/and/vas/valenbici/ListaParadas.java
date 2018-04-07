85cde532828d81889eefa80cb3b3926fe7682f91
Vasyl <vsmentvs@gmail.com>
2018-04-07 23:55:30 +0200

    Initial commit

---
+++ app/src/main/java/es/uv/and/vas/valenbici/ListaParadas.java
@@ -0,0 +1,13 @@
+package es.uv.and.vas.valenbici;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaParadas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_paradas);
+    }
+}
